400e0b1583f166509b5d7afde880ba3ad88d5fea
Patrick Moran <patrick.a.moran@gmail.com>
2011-12-13 23:31:00 -0500

    refs #4 - Add unit tests of the gap_buffer constructors

--- gap_buffer/gap_buffer_tests.cpp
+++ gap_buffer/gap_buffer_tests.cpp
@@ -119,5 +119,63 @@ void assert_position_end(TGapBuffer & buf)
 
 BOOST_AUTO_TEST_CASE(default_constructor)
 {
+  buffer_t default_constructed;
+  assert_properties_empty(default_constructed);
 }
+
+BOOST_AUTO_TEST_CASE(copy_constructor)
+{
+  buffer_t copied_value;
+  {
+    copied_value.insert('x');
+    assert_properties_size(copied_value, 1);
+  }
+  
+  buffer_t copy_constructed(copied_value);
+  assert_properties_size(copy_constructed, 1);
+  BOOST_CHECK(copy_constructed == copied_value);
+  BOOST_CHECK(!(copy_constructed != copied_value));
+
+  // Assert that absence of unintended aliasing
+  copy_constructed.insert('y');
+  BOOST_CHECK( copy_constructed != copied_value );
+}
+
+BOOST_AUTO_TEST_CASE(fill_constructor)
+{
+  size_t const first_len = 7;
+  buffer_t fill_constructed_1( first_len );
+  assert_properties_size( fill_constructed_1, first_len );
+  
+  buffer_t fill_constructed_2( first_len, '\0');
+  assert_properties_size( fill_constructed_1, first_len );
+  BOOST_CHECK( fill_constructed_1 == fill_constructed_2 );
+
+  { // Demonstrate its equivalence to another container's fill constructor
+    std::vector<char> other_cont(first_len, '\0');
+    BOOST_CHECK( seq_eq(fill_constructed_1, other_cont) );
+  }
+
+  size_t const third_len = 22;
+  char const third_fill_val = 'v';
+  buffer_t fill_constructed_3( third_len, third_fill_val );
+  assert_properties_size( fill_constructed_3, third_len );
+  { // Demonstrate its equivalence to another container's fill constructor
+    std::vector<char> other_cont(third_len, third_fill_val);
+    BOOST_CHECK( seq_eq(fill_constructed_3, other_cont) );
+  }
+  
+}
+
+BOOST_AUTO_TEST_CASE(iter_pair_constructor)
+{
+  static size_t const first_len = 22;
+  std::vector<char> other_cont(first_len, '\0');
+  
+  buffer_t iterator_constructed(other_cont.begin(), other_cont.end());
+  // Demonstrate that the data is all there
+  BOOST_CHECK( seq_eq(iterator_constructed, other_cont) );
+  assert_position_end( iterator_constructed );
+}
+
 BOOST_AUTO_TEST_SUITE_END()
